9654a4d99d6c7c35d5828a6d985d5744477bf87c
hlmeS <hsmidt@hawaii.edu>
2017-01-11 05:26:01 +0000

    updated gateway to add node to config

--- whttpd.py
+++ whttpd.py
@@ -34,8 +34,9 @@ import thread #, threading
 #from threading import Thread, enumerate
 import socket
 
+SER_DEBUG = True
+ 
 """ Config Parameters """
-global mac_addr, tempSet, valveLim, DR, pGain, iGain
 mac_addr = []
 tempSet = {}
 valveLim = {}
@@ -62,7 +63,6 @@ def init_config():
     db_usr = config.get('db_login', 'db_username')
     db_pass = config.get('db_login', 'db_pass')
     db_name = config.get('db_login', 'db_name')
-    #pinode_id = config.get('device_info', 'node')
 
 
 def init_ssh():
@@ -86,7 +86,6 @@ def ssh_to_db(node_mac, temp, valvePos):
     COMMAND += ' -h 127.0.0.1 -c '
     COMMAND += '"INSERT INTO nodes_data (node_mac, valvePos, temp) VALUES ('
     COMMAND += "'"+str(node_mac)+"'" + ', ' + str(valvePos) + ', ' + str(temp) + ')" '
-    print 'SSH_TO_DB: ' + COMMAND 
 
     try:
         ssh.exec_command(COMMAND)
@@ -94,30 +93,35 @@ def ssh_to_db(node_mac, temp, valvePos):
         init_ssh()
 
 def ssh_new_node(node):
-    """ SSH and add new mac address to the database.
-        It is up to the user change default configuration in the DB.
+    """ Append node to the list. 
+        It is up to the user to add it to the database
+	if it's not there yet. 
+	The user needs to change default configuration in the DB.
     """
     mac_addr.append(node)
-
-    tempSet[node] = 75.0
-    valveLim[node] = 1023
-    DR[node] = 1
-    pGain[node] = 0.04
-    iGain[node] = 0.02
-
+    
+    """ Query DB to get parameter 
     """
-    COMMAND  = 'PGPASSWORD=' + str(db_pass)
-    COMMAND += ' psql -U ' + str(db_usr)
-    COMMAND += ' -d ' + str(db_name)
-    COMMAND += ' -h 127.0.0.1 -c '
-    COMMAND += '"INSERT INTO nodes_config (node_mac) VALUES (' + str(node) +')"'
+    ssh_from_db(node)
+    
+    if not tempSet.has_key(node):
+        """ Make up config parameters and add to DB
+	"""
+	tempSet[node] = 75.0
+	valveLim[node] = 1023
+	DR[node] = 1
+	pGain[node] = 0.05
+	iGain[node] = 0.01
+	
+	COMMAND = 'PGPASSWORD=' + str(db_pass)
+	COMMAND += ' psql -U ' + str(db_usr)
+	COMMAND += ' -d ' + str(db_name)
+	COMMAND += ' -h 127.0.0.1 -c '
+	COMMAND += '"Insert INTO nodes_config (node_mac, tempset, valveLim, pgain, igain, dr) VALUES ('
+	COMMAND += "'"+str(node)+"'" + ', ' + str(tempSet[node]) + ',' + str(valveLim[node]) + ', '
+	COMMAND += str(DR[node]) + ',' + str(pGain[node]) + ',' + str(iGain[node]) +')" '
 
-    try:
-        ssh.exec_command(COMMAND)
-	print 'executed command'
-    except socket.error as e:
-        init_ssh()
-    """
+ 	
 
 def ssh_from_db(node):
     """ SSH and get configuration (DR parameters) from
@@ -130,7 +134,7 @@ def ssh_from_db(node):
     COMMAND += ' -h 127.0.0.1 -c '
     COMMAND += '"SELECT (tempSet, valveLim, pGain, iGain, dr) from nodes_config where node_mac='
     COMMAND += "'"+str(node) + "'"+ ' "'
-    print 'SSH_FROM_DB: ' + COMMAND
+    #print 'SSH_FROM_DB: ' + COMMAND
     try:
         stdin, stdout, stderr = ssh.exec_command(COMMAND)
         q_data = stdout.readlines()[2] #empty after reading
@@ -142,9 +146,9 @@ def ssh_from_db(node):
             pGain[node] = float(m[2])
             iGain[node] = float(m[3])
             DR[node] = int(m[4])
-	    print 'Node: ' + node + 'T: ' + str(tempSet[node]) + 'V: ' + str(valveLim[node]), 
-	    print 'pG: ' + str(pGain[node]) + 'iG: ' + str(iGain[node]) + 'DR: ' + str(DR[node])
-        #time.sleep(1)
+	    if SER_DEBUG: print 'Node: ' + node + ' T: ' + str(tempSet[node]) + ' V: ' + str(valveLim[node]), 
+	    if SER_DEBUG: print ' pG: ' + str(pGain[node]) + ' iG: ' + str(iGain[node]) + ' DR: ' + str(DR[node])
+        time.sleep(5)
     except socket.error as e:
         init_ssh()
 
@@ -174,15 +178,16 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.end_headers()
         p = re.compile('\?(.+?)!')
         m = p.findall(s.path)
-	print s.path
-	print m
+	if SER_DEBUG: print s.path
+	if SER_DEBUG: print m
 	node = m[0]
         if node in mac_addr:
             s.wfile.write("?" + str(DR[node]) + "!?" + str(tempSet[node]) + "!?" + str(valveLim[node]) + "!?" + str(pGain[node]) + "!?" + str(iGain[node]) + "!")
             ssh_to_db(node, float(m[1]), int(m[2]))
 
         else:
-            s.wfile.write("?!Adding mac address to DB\n") #not sure if needed
+	    outstring = "Adding mac address " + str(node) + " to the DB\n"
+            s.wfile.write(outstring) 		#not sure if needed
             ssh_new_node(node)
 
 
